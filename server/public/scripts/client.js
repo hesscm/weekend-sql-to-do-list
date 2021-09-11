48bdb09134a443abd59c824a4c00537bfec219d6
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-10 22:53:24 -0500

    added put and delete buttons/functions in the client. BASE MODE COMPLETE

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,10 +10,41 @@ function readyNow() {
 function handleClickEvents() {
     $('#submit-button').on('click', postListToServer);
     $('#to-do-list').on('click', '.complete-button', putMarkComplete);
+    $('#to-do-list').on('click', '.delete-button', deleteListItem);
+
+}
+
+function deleteListItem() {
+    console.log('in deleteListItem', $(this).data('id'));
+    let thisButton = $(this);
+    $.ajax({
+        method: 'DELETE',
+        url: `/list/${$(this).data('id')}`
+    })
+        .then(function (response) {
+            console.log('Deleted it!');
+            thisButton.parent().remove();
+            getListItems();
+        })
+        .catch(function (error) {
+            alert('Error on delete.', error);
+        })
 }
 
 function putMarkComplete() {
-console.log($(this).data('id'));
+    console.log('in putMarkComplete', $(this).data('id'));
+    
+    $.ajax({
+        method: 'PUT',
+        url: `/list/${$(this).data('id')}`
+    })
+        .then(function (response) {
+            console.log('Updated it!');
+            getListItems();
+        })
+        .catch(function (error) {
+            alert('Error on put.', error);
+        })
 }
 
 function postListToServer() {
@@ -50,24 +81,38 @@ function getListItems() {
 
 function appendListToDom(list) {
     console.table(list);
-    //change boolean to yes or no response
-
+    //variables to track item completion
     let isComplete = '';
+    let addCheckMark = ''; 
+
+    $('#to-do-list').empty(); //empty the list on DOM
+    
+    //iterate through the list
     for (let i = 0; i < list.length; i++) {
+        //if this item is completed...
         if (list[i].isComplete === true) {
-            isComplete = 'Yes'
+            isComplete = 'Yes' //text to appear on DOM
+            completeClass = 'isComplete'; //CSS class to change style
+            addCheckMark = '&check;' //HTML entity code for a checkmark
         } else {
             isComplete = 'No';
+            completeClass = 'isNotComplete';
+            addCheckMark = '';
         }
         //append list to DOM
         $('#to-do-list').append(`
-        <tr>
+        <tr class="${completeClass}">
             <td>${list[i].note}</td>
             <td>${list[i].category}</td>
             <td>${list[i].priority}</td>
             <td>${isComplete}</td>
             <td>${list[i].timeCompleted}</td>
-            <td><button class="complete-button" data-id="${list[i].id}">Mark Complete</button</td>
+            <td>${addCheckMark}
+                <button class="complete-button" data-id="${list[i].id}">Mark Complete</button
+            </td>
+            <td>
+                <button class="delete-button" data-id="${list[i].id}">Delete Item</button
+                </td>
         </tr>
         `)
     }
